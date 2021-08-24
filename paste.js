6289b61a4790d41fff775740ae920d2d04e0778f
imagineeeinc <imagineeelego@gmail.com>
2021-08-24 17:08:41 +0100

    added abyss theme

---
+++ paste.js
@@ -0,0 +1,8 @@
+const fs = require('fs-extra')
+
+// With a callback:
+fs.copy('./.vimrc','../../.vimrc', (err) => {
+  if (err) return console.error(err)
+  console.log('success!')
+}) // copies file
+
